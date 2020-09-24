d8b6165c14b91be8cf508eb2002870b17537c6ff
Erik Butcher <with_a_k@mac.com>
2020-09-24 13:49:30 -0600

    Adjust PHP tags

--- web/navi.php
+++ web/navi.php
@@ -1,10 +1,9 @@
-<?php
-echo '<h1>Erik's Page</h1>
-<p>The current time is ' . date(h:i a) . '.; ?></p>
+
+<h1>Erik's Page</h1>
+<p>The current time is <?php echo date(h:i a) . "."; ?></p>
 <div>
   <a href="index.php">Index</a>
 </div>
 <div>
   <a href="projects.php">Projects</a>
-</div>';
-?>
+</div>
