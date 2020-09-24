b04e5fcf66b059772ef7025f82513e21bacd8e49
Erik Butcher <with_a_k@mac.com>
2020-09-24 14:24:43 -0600

    Basic navbar styling

--- web/navi.php
+++ web/navi.php
@@ -1,10 +1,10 @@
 <html>
-<h1>Erik's Page</h1>
-<p>The current time is <?php echo date("h:i a"); ?>.</p>
-<div>
-  <a href="index.php">Index</a>
+<div class="grid-x">
+  <h1 class="text-center">Erik's Page</h1>
+  <p class="text-center">The current time is <?php echo date("h:i a"); ?>.</p>
 </div>
-<div>
-  <a href="projects.php">Projects</a>
-</div>
+<ul class="menu expanded align-center">
+  <li><a href="index.php">Index</a></li>
+  <li><a href="projects.php">Projects</a></li>
+</ul>
 </html>
