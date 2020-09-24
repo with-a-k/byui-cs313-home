466f81d4d5effb6aa8c73f2a32941c778b61668c
Erik Butcher <with_a_k@mac.com>
2020-09-24 14:02:11 -0600

    Check PHP installation

--- web/index.php
+++ web/index.php
@@ -1,12 +1,5 @@
-<html>
-  <head>
-    <title>Erik's Homepage</title>
-    <link rel=stylesheet href="styles/foundation.css">
-  </head>
-  <body>
-    <div>
-      <?php include "navi.php"; ?>
-    </div>
+<?php
 
-  </body>
-</html>
+phpinfo();
+
+?>
