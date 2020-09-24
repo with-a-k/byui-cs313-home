c3537812ec227ae9f7229ca74a41900db1feee03
Erik Butcher <with_a_k@mac.com>
2020-09-24 14:08:23 -0600

    Fix call to date

--- web/navi.php
+++ web/navi.php
@@ -1,6 +1,6 @@
 <html>
 <h1>Erik's Page</h1>
-<p>The current time is <?php echo date(h:i a); ?>.</p>
+<p>The current time is <?php echo date("h:i a"); ?>.</p>
 <div>
   <a href="index.php">Index</a>
 </div>
