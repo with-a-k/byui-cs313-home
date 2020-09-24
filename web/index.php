22c89d5e53ca74df4826ddc18dc2237a4d8b4e65
Erik Butcher <with_a_k@mac.com>
2020-09-24 11:41:39 -0600

    Create index skeleton, navbar

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <head>
+    Erik's Homepage
+    <link rel=stylesheet href="styles/foundation.css">
+  </head>
+  <body>
+    <div>
+      <?php include "navi.php"; ?>
+    </div>
+
+  </body>
+</html>
---
+++ web/navi.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <body>
+    <h1>Erik's Page</h1>
+    <p>The current time is <? php echo "The current time is " . date(h:i a) . "."; ?></p>
+    <div>
+      <a href="index.php">Index</a>
+    </div>
+    <div>
+      <a href="projects.php">Projects</a>
+    </div>
+  </body>
+</html>
