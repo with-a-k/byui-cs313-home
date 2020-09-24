31f4760a6b0dc471ca7222df0a2c1025a5f119e4
Erik Butcher <with_a_k@mac.com>
2020-09-24 14:27:53 -0600

    Add styles

--- web/navi.php
+++ web/navi.php
@@ -1,6 +1,6 @@
 <html>
 <div class="grid-x">
-  <h1 class="text-center">Erik's Page</h1>
+  <p class="h1 text-center">Erik's Page</h1>
   <p class="text-center">The current time is <?php echo date("h:i a"); ?>.</p>
 </div>
 <ul class="menu expanded align-center">
