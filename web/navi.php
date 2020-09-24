0dafae153de1c3be9f1e8d65fa8ccd95b21e8b58
Erik Butcher <with_a_k@mac.com>
2020-09-24 14:06:00 -0600

    Wrap navbar in HTML tags

--- web/navi.php
+++ web/navi.php
@@ -1,9 +1,10 @@
-
+<html>
 <h1>Erik's Page</h1>
-<p>The current time is <?php echo date(h:i a) . "."; ?></p>
+<p>The current time is <?php echo date(h:i a); ?>.</p>
 <div>
   <a href="index.php">Index</a>
 </div>
 <div>
   <a href="projects.php">Projects</a>
 </div>
+</html>
