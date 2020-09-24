e7f69c985ff614b4798cdf46a1be82196f77d936
Erik Butcher <with_a_k@mac.com>
2020-09-24 13:31:03 -0600

    Make navbar all PHP

--- web/navi.php
+++ web/navi.php
@@ -1,8 +1,10 @@
-<h1>Erik's Page</h1>
-<p>The current time is <? php echo "The current time is " . date(h:i a) . "."; ?></p>
+<?php
+echo '<h1>Erik's Page</h1>
+<p>The current time is ' . date(h:i a) . '.; ?></p>
 <div>
   <a href="index.php">Index</a>
 </div>
 <div>
   <a href="projects.php">Projects</a>
-</div>
+</div>';
+?>
