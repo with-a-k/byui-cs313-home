0f941278e6e552ecbca74006afe16e88ec106e45
Erik Butcher <with_a_k@mac.com>
2020-09-24 22:25:34 -0600

    Create project page

--- web/index.php
+++ web/index.php
@@ -8,6 +8,9 @@
     <div>
       <?php include 'navi.php'; ?>
     </div>
-
+    <p class="text-center">
+      We all walk long and winding roads. Reach out to each other for comfort.
+      This is my little post in the storm. Take a load off, and maybe I can help with it.
+    </p>
   </body>
 </html>
---
+++ web/projects.php
@@ -0,0 +1,33 @@
+<html>
+  <head>
+    <title>Erik's Homepage</title>
+    <link rel=stylesheet href="styles/foundation.css">
+    <link rel=stylesheet href="styles/personal-styles.css">
+  </head>
+  <body>
+    <div>
+      <?php include 'navi.php'; ?>
+    </div>
+    <p class="text-center">Here's some things I've made. Or will make.</p>
+    <div class="card-container grid-container fluid">
+      <div class="grid-x">
+        <div class="card cell small-5">
+          <div class="card-divider">
+            TableTime
+          </div>
+          <div class="card-section">
+            Coordinating meetings is hard. I'm creating TableTime to help make it easier, no matter how many time zones you're dealing with.
+          </div>
+        </div>
+        <div class="card cell small-5">
+          <div class="card-divider">
+            Valor Character Builder
+          </div>
+          <div class="card-section">
+            An unofficial tool for creating characters for Valorous Games' "Valor: the Heroic Roleplay System".
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
