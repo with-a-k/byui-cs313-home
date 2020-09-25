40bda5d61bbca7b52dcea75a3277f41c3b607444
Erik Butcher <with_a_k@mac.com>
2020-09-24 22:43:46 -0600

    Fix specificity rules

--- web/projects.php
+++ web/projects.php
@@ -9,21 +9,21 @@
       <?php include 'navi.php'; ?>
     </div>
     <p class="text-center">Here's some things I've made. Or will make.</p>
-    <div class="card-container grid-container fluid">
-      <div class="grid-x grid-margin-1">
-        <div class="card cell small-4">
-          <div class="card-divider">
+    <div class="card-container grid-container">
+      <div class="grid-x grid-margin-2">
+        <div class="card cell small-4 project">
+          <div class="card-divider project">
             TableTime
           </div>
-          <div class="card-section">
+          <div class="card-section project">
             Coordinating meetings is hard. I'm creating TableTime to help make it easier, no matter how many time zones you're dealing with.
           </div>
         </div>
-        <div class="card cell small-4">
-          <div class="card-divider">
+        <div class="card cell small-4 project">
+          <div class="card-divider project">
             Valor Character Builder
           </div>
-          <div class="card-section">
+          <div class="card-section project">
             An unofficial tool for creating characters for Valorous Games' "Valor: the Heroic Roleplay System".
           </div>
         </div>
