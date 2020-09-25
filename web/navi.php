9307051bcbc2d06f1b25686c7024745d291457d0
Erik Butcher <with_a_k@mac.com>
2020-09-24 22:04:28 -0600

    Add contrast and background image

--- web/navi.php
+++ web/navi.php
@@ -1,5 +1,7 @@
+<?php date_default_timezone_set("America/Denver"); ?>
+
 <html>
-  <p class="h1 text-center">Erik's Page</h1>
+  <p class="h1 subheader text-center">Erik's Page</h1>
   <p class="text-center">The current time is <?php echo date("h:i a"); ?>.</p>
 <ul class="menu expanded align-center">
   <li><a href="index.php">Index</a></li>
